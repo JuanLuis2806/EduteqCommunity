5d2cc92964a1b6fa2e543554d71fd22dd607244f
JDanielHM <59979542+JDanielHM@users.noreply.github.com>
2021-02-16 19:20:34 -0600

    Mercado

---
+++ src/main/webapp/js/mercado.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    const imagen = $("#imagen-producto"),
+            nombre = $("#nombre-producto"),
+            descripcion = $("#descripcion-producto"),
+            precio = $("#precio-producto");
+
+    $("#subir-producto").on("submit", function (e) {
+        e.preventDefault();
+        const data = new FormData();
+        data.append("imagen", imagen[0].files[0]);
+        data.append("nombre", nombre.val());
+        data.append("descripcion", descripcion.val());
+        data.append("precio", precio.val());
+
+        $.ajax({
+            type: 'POST',
+            url: "agregarProducto",
+            data: data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            beforeSend: function (xhr) {},
+            success: function (data) {
+                if ("OK" === data) {
+                    alert("El producto fue agregado!!!")
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("Esto esta mal qlo!")
+            }
+        });
+    });
+});
